bb3156634fa083e55392bbf697d2f865c2138c7f
Joey Smith <jsmith@webinertia.net>
2023-06-26 20:56:38 -0500

    Omitted changes to LogListener Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/LogListener.php
+++ src/LogListener.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Webinertia\Log;
 
-use Webinertia\Log\LogEvent;
-use Webinertia\Log\LoggerAwareInterface;
+use Exception;
 use Laminas\EventManager\EventInterface;
 use Laminas\EventManager\EventManagerInterface;
 use Laminas\EventManager\ListenerAggregateInterface;
@@ -14,7 +13,12 @@ use Laminas\Mvc\MvcEvent;
 use Laminas\Mvc\I18n\Translator;
 use Laminas\I18n\Translator\TranslatorAwareInterface;
 use Laminas\I18n\Translator\TranslatorAwareTrait;
+use Throwable;
 use Psr\Log\LoggerInterface;
+use Webinertia\Log\LogEvent;
+use Webinertia\Log\LoggerAwareInterface;
+
+use function assert;
 
 final class LogListener implements ListenerAggregateInterface, TranslatorAwareInterface
 {
@@ -27,15 +31,20 @@ final class LogListener implements ListenerAggregateInterface, TranslatorAwareIn
     /** @var Translator $translator */
     protected $translator;
 
-    public function __construct(LoggerInterface $logger, Translator $translator, array $config)
-    {
+    public function __construct(
+        LoggerInterface $logger,
+        Translator $translator,
+        protected array $config
+    ) {
         $this->setLogger($logger);
         $this->translator = $translator;
-        //
-        // if ($config['server']['log_errors']) {
-        //     $log = $this->logger->getLogger();
-        //     $log::registerErrorHandler($log, true);
-        // }
+        if (
+            isset($config['app_setting']['log_settings'])
+            && $config['app_settings']['log_settings']['log_errors']
+        ) {
+            $log = $this->logger->getLogger();
+            $log::registerErrorHandler($log, true);
+        }
     }
 
     /** @inheritDoc */
@@ -44,48 +53,56 @@ final class LogListener implements ListenerAggregateInterface, TranslatorAwareIn
         $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH_ERROR, [$this, 'log']);
 
         $sharedMananger    = $events->getSharedManager();
+        // code 0
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::EMERGENCY,
             [$this, 'log'],
             $priority
         );
+        // code 1
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::ALERT,
             [$this, 'log'],
             $priority
         );
+        // code 2
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::CRITICAL,
             [$this, 'log'],
             $priority
         );
+        // code 3
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::ERROR,
             [$this, 'log'],
             $priority
         );
+        // code 4
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::WARNING,
             [$this, 'log'],
             $priority
         );
+        // code 5
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::NOTICE,
             [$this, 'log'],
             $priority
         );
+        // code 6
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::INFO,
             [$this, 'log'],
             $priority
         );
+        // code 7
         $this->listeners[] = $sharedMananger->attach(
             LoggerAwareInterface::class,
             LogEvent::DEBUG,
@@ -100,23 +117,40 @@ final class LogListener implements ListenerAggregateInterface, TranslatorAwareIn
         $name        = $event->getName();
         $logMessage  = $event->getTarget();
         $params      = $event->getParams();
-        $error       = $event->getError();
-        $ex          = $params['exception'] ?? null;
-        if ($params !== []) {
-            $passContext = true;
+
+        if ($event instanceof LogEvent) {
+            if ($params !== []) {
+                $passContext = true;
+            }
+            if ($passContext) {
+                unset($params['translate']);
+                $this->logger->$name($this->getTranslator()->translate($logMessage), $params);
+            } else {
+                unset($params);
+                $this->logger->$name($this->getTranslator()->translate($logMessage));
+            }
         }
-        if ($passContext) {
-            unset($params['translate']);
-            $this->logger->$name($this->getTranslator()->translate($logMessage), $params);
-        } else {
-            unset($params);
-            $this->logger->$name($this->getTranslator()->translate($logMessage));
-        }
-        if ($error !== null) {
-            // we have an error
-        }
-        if ($ex !== null) {
-            // we have an exception
+        if ($event instanceof MvcEvent) {
+            assert($event instanceof MvcEvent);
+            /** @var Exception $ex */
+            $ex = $params['exception'];
+
+            if ($ex instanceof Throwable || $ex instanceof Exception) {
+                /**
+                 * since we can only send a int code via exception classes lets switch to
+                 * the standard laminas logger so we can pass a $priority
+                 */
+                $logger = $this->logger->getLogger();
+                $logger->log(
+                    $ex->getCode(),
+                    $ex->getMessage(),
+                    [
+                        'file'  => $ex->getFile(),
+                        'line'  => $ex->getLine(),
+                        'trace' => $ex->getTraceAsString(),
+                    ]
+                );
+            }
         }
     }
 }
